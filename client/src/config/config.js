e3067a1da051ab7ad681c44b57a0f6843c1f8897
tolgahanbora <tolgahanbora@outlook.com>
2023-04-12 22:27:51 +0300

    added

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://tolgahanbora-ai-tshirtmaker.onrender.com",
+    backendUrl: "https://tolgahanbora-ai-tshirtmaker.onrender.com/api/v1/dalle",
   },
 };
 
